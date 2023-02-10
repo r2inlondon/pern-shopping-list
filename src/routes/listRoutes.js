04a36273e48ed82d99dedb1066c0f0ab85201133
Arturo Quiroz <r2inlondon@gmail.com>
2023-02-10 22:05:55 +0000

    feat : delete list route implemented

--- src/routes/listRoutes.js
+++ src/routes/listRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { getLists, newList } = require("../controllers/listController");
+const {
+  getLists,
+  newList,
+  deleteList,
+} = require("../controllers/listController");
 
 // List routes
 router.get("/", getLists);
 router.post("/new", newList);
+router.delete("/", deleteList);
 
 module.exports = router;
