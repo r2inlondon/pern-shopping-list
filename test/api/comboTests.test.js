396dbd770219f9239e182301120b6d40d79984de
Arturo Quiroz <r2inlondon@gmail.com>
2023-02-10 22:34:50 +0000

    test : delete list and user added to comboTest suit

--- test/api/comboTests.test.js
+++ test/api/comboTests.test.js
@@ -7,7 +7,7 @@ describe("Combining tests", () => {
   let comboListObj;
 
   it("ComboTest get user Create new user", async () => {
-    const results = await request(app)
+    await request(app)
       .post("/user/new")
       .send({
         firstName: "Emiliano",
@@ -31,14 +31,46 @@ describe("Combining tests", () => {
   });
 
   it("ComboTest create List", async () => {
-    comboListObj = await request(app)
+    await request(app)
       .post("/lists/new")
       .send({
         userId: comboUserObj.id,
         name: "gigante",
       })
       .then((res) => {
+        comboListObj = res.body;
         expect(res.body.name).toBe("Gigante");
+      });
+  });
+
+  it("Get all list", async () => {
+    await request(app)
+      .get("/lists")
+      .send({
+        userId: comboUserObj.id,
+      })
+      .then((res) => {
+        expect(res.body.length).toBe(1);
+      });
+  });
+
+  it("Delete List", async () => {
+    await request(app)
+      .delete("/lists")
+      .send({
+        id: comboListObj.id,
+      })
+      .then((res) => expect(res.body.name).toBe("Gigante"));
+  });
+
+  it("Delete Emiliano", async () => {
+    await request(app)
+      .delete("/user")
+      .send({
+        email: comboUserObj.email,
+      })
+      .then((res) => {
+        expect(res.statusCode).toBe(201);
       });
   });
 });
