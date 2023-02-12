0e033f26842090129e862bc62ece56b093b5f4d6
Arturo Quiroz <r2inlondon@gmail.com>
2023-02-12 20:42:55 +0000

    fix : added ID in shopping, to be able to delete rows in shopping

--- src/routes/shoppingRoutes.js
+++ src/routes/shoppingRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const {
   getAllProductsFromList,
   newShoppingItem,
+  deleteShoppingItem,
 } = require("../controllers/shoppingController");
 
 router.get("/", getAllProductsFromList);
 router.post("/new", newShoppingItem);
+router.delete("/", deleteShoppingItem);
 
 module.exports = router;
