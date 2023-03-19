e7ee6e46251a6869f9b183e933dba9b35af37101
Arturo <53298840+r2inlondon@users.noreply.github.com>
2023-03-19 20:25:34 +0000

    updated path when requiring app

--- test/api/comboTests.test.js
+++ test/api/comboTests.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const { app } = require("../../src/routes/routes");
+const { app } = require("../../app");
 
 describe("Combining tests", () => {
   let authTokens;
